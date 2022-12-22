2e46d0864693851104683c2cdf3ef275211356fe
taaru <bfflei1@gmail.com>
2022-12-22 14:23:35 +0700

    Update cors.config.js

--- backend/src/configs/cors.config.js
+++ backend/src/configs/cors.config.js
@@ -4,10 +4,7 @@ const app = express();
 
 const corsConfig = {
   // Configures the Access-Control-Allow-Origin
-  origin:
-    app.get('env') !== 'production'
-      ? 'http://localhost:3000'
-      : process.env.CORS_ORIGIN,
+  origin:process.env.CORS_ORIGIN,
 
   // Configures the Access-Control-Allow-Methods
   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
